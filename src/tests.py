5a59b7892bffcfca4da6b262406ddd2785707ea1
jgc13 <jgc13@sfu.ca>
2023-12-05 17:38:35 -0800

    testing both ports

--- src/tests.py
+++ src/tests.py
@@ -17,43 +17,31 @@ def send_malformed_request():
     
 
 
-url = "http://localhost:8888"
-def test200(): # Request that works
+
+def test200(url): # Request that works
     print("Testing Response Code 200:")
     result = requests.get(url)
     print("Resulting Code : " + str(result.status_code) + " Expected Code: 200\n")
     return result.status_code == 200
-def test304(): # Request that hasn't been modified
-    print("Testing Response Code 304:")
-    
-    s = Session()
 
-    req = Request('POST', url)
-    midwayPoint = req.prepare()
-    midwayPoint.headers['If-Modified-Since'] = strftime("%a, %d %b %Y %H:%M:%S",datetime.now())
-    print(midwayPoint.headers)
-    
-    result = s.send(midwayPoint)
-    return 
-def test403(): # Testing request to forbidden path
+def test403(url): # Testing request to forbidden path
     print("Testing Response Code 403:")
     result = requests.get(url+ "/forbiddenpath")
     print("Resulting Code : " + str(result.status_code) + " Expected Code: 403\n")
     return result.status_code == 403
-def test404(): # Test response when requesting a nonexistent resource
+def test404(url): # Test response when requesting a nonexistent resource
     print("Testing Response Code 404:")
     result = requests.get(url + "/8thwonderoftheworld")
     print("Resulting Code : " + str(result.status_code) + " Expected Code: 404\n")
     return result.status_code == 404
-def test411(): # Test response when content length is not given
-    print("Testing Response Code 411:")
-    
-    print("Resulting Code : " + str(result.status_code) + " Expected Code: 411\n")
-    return result.status_code == 411
+
 if __name__ == "__main__":
-    print("test200 Passed? : " + str(test200()))
-    print("test304 Passed? : " + str(test304()))
-    print("test403 Passed? : " + str(test403()))
-    print("test404 Passed? : " + str(test404()))
-    print("test411 Passed? : " + str(test411()))
+    for i in ["8080","8888"]:
+        print("testing with port: " + i)
+        print("--------------------------")
+        url = "http://localhost:" + i
+        print("test200 Passed? : " + str(test200(url)))
+        print("test403 Passed? : " + str(test403(url)))
+        print("test404 Passed? : " + str(test404(url)))
+        print("--------------------------")
     send_malformed_request()
