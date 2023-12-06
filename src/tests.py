412801ad445744ae6b7cf08a30fdcd3c6f4276c9
jgc13 <jgc13@sfu.ca>
2023-12-05 17:00:04 -0800

    Makin Tests

--- src/tests.py
+++ src/tests.py
@@ -1,32 +1,53 @@
 import requests
 from requests import Request, Session
+import socket
+from datetime import datetime
+from time import strftime
+
+def send_malformed_request():
+    try:
+        with socket.create_connection(('localhost', 8080)) as s:
+            # Send a malformed request
+            s.sendall(b'GET\n\n')
+            response = s.recv(1024)
+            print("Testing Response Code 400:")
+            print("Passed Test? " +  str(("400" in response.decode())))
+    except Exception as e:
+        print(f"Error: {e}")
+    
+
+
 url = "http://localhost:8888"
 def test200(): # Request that works
-    print("Running test200:")
+    print("Testing Response Code 200:")
     result = requests.get(url)
     print("Resulting Code : " + str(result.status_code) + " Expected Code: 200\n")
     return result.status_code == 200
 def test304(): # Request that hasn't been modified
-    print("Running test304:")
-    return 
-def test403(): # Testing request to forbidden path
-    print("Running test403:")
-    result = requests.get(url+ "/forbiddenpath")
-    print("Resulting Code : " + str(result.status_code) + " Expected Code: 403\n")
-    return result.status_code == 403
-def test404(): # Test response when requesting a nonexistent resource
-    print("Running test404:")
-    result = requests.get(url + "/8thwonderoftheworld")
-    print("Resulting Code : " + str(result.status_code) + " Expected Code: 404\n")
-    return result.status_code == 404
-def test411(): # Test response when content length is not given
-    print("Running test411:")
+    print("Testing Response Code 304:")
+    
     s = Session()
 
     req = Request('POST', url)
     midwayPoint = req.prepare()
-    del midwayPoint.headers['Content-Length']
+    midwayPoint.headers['If-Modified-Since'] = strftime("%a, %d %b %Y %H:%M:%S",datetime.now())
+    print(midwayPoint.headers)
+    
     result = s.send(midwayPoint)
+    return 
+def test403(): # Testing request to forbidden path
+    print("Testing Response Code 403:")
+    result = requests.get(url+ "/forbiddenpath")
+    print("Resulting Code : " + str(result.status_code) + " Expected Code: 403\n")
+    return result.status_code == 403
+def test404(): # Test response when requesting a nonexistent resource
+    print("Testing Response Code 404:")
+    result = requests.get(url + "/8thwonderoftheworld")
+    print("Resulting Code : " + str(result.status_code) + " Expected Code: 404\n")
+    return result.status_code == 404
+def test411(): # Test response when content length is not given
+    print("Testing Response Code 411:")
+    
     print("Resulting Code : " + str(result.status_code) + " Expected Code: 411\n")
     return result.status_code == 411
 if __name__ == "__main__":
@@ -35,3 +56,4 @@ if __name__ == "__main__":
     print("test403 Passed? : " + str(test403()))
     print("test404 Passed? : " + str(test404()))
     print("test411 Passed? : " + str(test411()))
+    send_malformed_request()
